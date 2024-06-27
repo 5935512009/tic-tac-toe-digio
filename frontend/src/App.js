6af90fb9e02beea4cd4b5faef69a2c7979edddc7
5935512009 <Pasut2541@hotmail.com>
2024-06-27 09:59:39 +0700

    new:{front end tic-tac-toe}

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      fisrt commit tic-tac-toe 27/06/2024
+    </div>
+  );
+}
+
+export default App;
