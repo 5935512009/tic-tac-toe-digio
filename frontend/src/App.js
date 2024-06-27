14c032d4d844bba5ea3c2fdd19285eb1a02dc1fa
5935512009 <Pasut2541@hotmail.com>
2024-06-27 14:54:07 +0700

    update:{add page}

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import TicTactoe from './components/ticTacToe/TicTactoe';
 
 function App() {
   return (
     <div className="App">
       fisrt commit tic-tac-toe 27/06/2024
+      <TicTactoe/>
     </div>
   );
 }
