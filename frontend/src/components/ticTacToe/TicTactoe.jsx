b74335c0c6ed6389341175fa7af78860e11fa231
5935512009 <Pasut2541@hotmail.com>
2024-06-29 11:03:04 +0700

    update:{basic xo}

--- frontend/src/components/ticTacToe/TicTactoe.jsx
+++ frontend/src/components/ticTacToe/TicTactoe.jsx
@@ -1,34 +1,77 @@
 import React,{useState} from 'react'
 import './tictactoe.css'
-function Square(){
-    const [value, setValue] = useState(null);
-
-    function handleClick(){
-        setValue('x');
-    }
-     return <button className="square" onClick={handleClick}>{value}</button>
+function Square({value, onSquareClick}){
+    
+     return <button className="square" onClick={onSquareClick} >{value}</button>
 }
 
 export default function TicTactoe() {
+    const [square,setSquare] = useState(Array(9).fill(null));
+    const [xIsNext, setXIsNext] = useState(true);
+    const winner = calculateWinner(square);
+    let status;
+    if(winner){
+        status ="Winner: " + winner;
+    }else{
+        status ="Next player: "+ (xIsNext?"x":"O");
+    }
+
+
+    function handleClick(i){
+        if(square[i] || calculateWinner(square)){
+            return; // check square ค่าใหม ถ้ามีจะ return ทำให้คลิกซ้ำไม่ได้
+        }
+        const nextSqure = square.slice(); // copy array
+        if(xIsNext){
+            nextSqure[i] = "X";
+        }
+        else{
+            nextSqure[i] = "O";
+        }
+        setSquare(nextSqure);
+        setXIsNext(!xIsNext)
+    }
+
   return (
-    <div>
+    <div className="ticTactoe">
+        <div className="status">{status}</div>
         <div className="wrapped">
-           <Square />
-           <Square />
-           <Square />
+           <Square value={square[0]} onSquareClick={() => handleClick(0)} />
+           <Square value={square[1]} onSquareClick={()=>handleClick(1)}/>
+           <Square value={square[2]} onSquareClick={()=>handleClick(2)}/>
         </div>
         <div className="wrapped">     
-            <Square />
-            <Square />
-            <Square />
+            <Square value={square[3]} onSquareClick={() => handleClick(3)}/>
+            <Square value={square[4]} onSquareClick={() => handleClick(4)}/>
+            <Square value={square[5]} onSquareClick={() => handleClick(5)}/>
         </div>
         <div className="wrapped">
-            <Square />
-            <Square />
-            <Square />
+            <Square value={square[6]} onSquareClick={() => handleClick(6)}/>
+            <Square value={square[7]} onSquareClick={() => handleClick(7)}/>
+            <Square value={square[8]} onSquareClick={() => handleClick(8)}/>
         </div>
 
 
     </div>
   )
+}
+
+function calculateWinner(square){
+    const lines=[
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+        [0,4,8],
+        [2,4,6]
+    ];
+    for(let i=0;i<lines.length;i++){
+        const [a,b,c] =lines[i]
+        if(square[a]&&square[a] === square[b] && square[a] === square[c]){
+            return square[a];
+        }
+    }
+    return null;
 }
