475a3f2ab5341a6d87d445a0d325831a75564b3d
5935512009 <Pasut2541@hotmail.com>
2024-07-01 17:07:34 +0700

    update:{i j one click bug}

--- frontend/src/components/ticTacToe/TictactoeInputTest.jsx
+++ frontend/src/components/ticTacToe/TictactoeInputTest.jsx
@@ -1,25 +1,41 @@
 import React,{useState} from 'react'
+import './tictactoeInputTest.css'
+
 
 export default function TictactoeInputTest() {
 
     const [boardSide,setBoardSide] = useState(3);
+    const [board,setBoard] = useState([]);
+    const [value,setValue] = useState(null);
+//-------------------onClick event ------------------------//
+function handleClick(){
+    setValue('O');
+}
+
+
 //----------------------รับค่าจาก input มา หลังจากนั้นเอาเข้า function setBoardSide -------------------------//    
     const handleInputChange = (event) =>{
         setBoardSide(event.target.value);
     }
 //----------------------หลังกด submit event จะทำงาน-------------------------//
     const handleSubmit = ()=>{
+         const newBoard = []; // newboard รับค่า array i j  เพื่อกำหนดตำแหน่ง Square
         if(boardSide){
+            
             for(let i=0;i<boardSide;i++){
+                const row = []; // ตัวแปรรับ Square ในแต่ละ row
                 for(let j=0;j<boardSide;j++){
                     console.log(`[${i}, ${j}]`);
+                    row.push(<Square key={`${i}-${j}`} />); // .push ต้อข้อความไปเรื่อยๆใน ตัวแปร row
                 }
+                newBoard.push(<div key={i} className="board-row">{row}</div>); // หลังหลุด loop j จะเก็บข้อมูลใน newBoard และทำการขึ้นแถวใหม่ตาม css 
             }
          }
+         setBoard(newBoard); // เก็บค่า newBoard ในตัวแปร board
     }
-    function Square({value}){
+    function Square(){
     
-        return <button className="square" >hello</button>
+        return <button className="square" onClick={handleClick}>{value}</button> // แสดงค่า x,o
    }
     
   return (
@@ -27,8 +43,14 @@ export default function TictactoeInputTest() {
         <br/>
         <input type="number" name="" id="" value={boardSide} onChange={handleInputChange}/>
       <button type="submit" onClick={handleSubmit}>submit</button>
-      
-      <Square/>
+      <div className='TictactoeInputTest-board'>
+        {board.map((board,index)=>(
+            <div key={index} >
+                {board}
+            </div>
+        ))}
+      </div>
+        
     </div>
   )
 }
