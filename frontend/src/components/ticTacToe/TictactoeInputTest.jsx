170c2030336618b5d58c574ea87b816f024a920c
5935512009 <Pasut2541@hotmail.com>
2024-07-03 11:55:49 +0700

    update:{unlimit value o}

--- frontend/src/components/ticTacToe/TictactoeInputTest.jsx
+++ frontend/src/components/ticTacToe/TictactoeInputTest.jsx
@@ -6,18 +6,14 @@ export default function TictactoeInputTest() {
 
     const [boardSide,setBoardSide] = useState(3);
     const [board,setBoard] = useState([]);
-    const [value,setValue] = useState(null);
-//-------------------onClick event ------------------------//
-function handleClick(){
-    setValue('O');
-}
 
 
 //----------------------รับค่าจาก input มา หลังจากนั้นเอาเข้า function setBoardSide -------------------------//    
     const handleInputChange = (event) =>{
         setBoardSide(event.target.value);
     }
-//----------------------หลังกด submit event จะทำงาน-------------------------//
+
+//----------------------กด submit จะทำการสร้าง board ขึ้นมาตามจำนวน-------------------------//
     const handleSubmit = ()=>{
          const newBoard = []; // newboard รับค่า array i j  เพื่อกำหนดตำแหน่ง Square
         if(boardSide){
@@ -26,16 +22,30 @@ function handleClick(){
                 const row = []; // ตัวแปรรับ Square ในแต่ละ row
                 for(let j=0;j<boardSide;j++){
                     console.log(`[${i}, ${j}]`);
-                    row.push(<Square key={`${i}-${j}`} />); // .push ต้อข้อความไปเรื่อยๆใน ตัวแปร row
+                    row.push({id:`${i}-${j}`,value:null}); // .push ต้อข้อความไปเรื่อยๆใน ตัวแปร row
                 }
-                newBoard.push(<div key={i} className="board-row">{row}</div>); // หลังหลุด loop j จะเก็บข้อมูลใน newBoard และทำการขึ้นแถวใหม่ตาม css 
+                newBoard.push(row); // หลังหลุด loop j จะเก็บข้อมูลใน newBoard และทำการขึ้นแถวใหม่ตาม css 
             }
          }
          setBoard(newBoard); // เก็บค่า newBoard ในตัวแปร board
     }
-    function Square(){
+//------------------------------------------------------//
+const handleSquareClick = (rowIndex,colIndex)=>{
+    const newBoard = board.map((row,i)=>
+        row.map((square,j)=>{
+            if( i===rowIndex && j===colIndex && !square.value) // i===rowIndex ดู index i เป็นตัวแปรชนิดเดียวกับ rowIndex ใหม , j===colIndex ดู index j เป็นตัวแปรชนิดเดียวกับ colIndex ใหม , !square.value  เช็คว่าช่องนั้นๆ ว่างอยู่หรือไม่ก่อนที่เปลี่ยนค่าในช่องนั้น
+            {
+                return {...square, value:'O'}; // set ค่า value
+            }
+            return square;
+        })
+    );
+    setBoard(newBoard);
+}
+
+    function Square({value, onClick}){
     
-        return <button className="square" onClick={handleClick}>{value}</button> // แสดงค่า x,o
+        return <button className="square" onClick={onClick}>{value}</button> // แสดงค่า x,o
    }
     
   return (
@@ -44,9 +54,15 @@ function handleClick(){
         <input type="number" name="" id="" value={boardSide} onChange={handleInputChange}/>
       <button type="submit" onClick={handleSubmit}>submit</button>
       <div className='TictactoeInputTest-board'>
-        {board.map((board,index)=>(
-            <div key={index} >
-                {board}
+        {board.map((row,rowIndex)=>(
+            <div key={rowIndex} >
+                {row.map((square,colIndex)=>(
+                    <Square 
+                        key={square.id}
+                        value={square.value}
+                        onClick={()=>handleSquareClick(rowIndex,colIndex)}
+                    />
+                ))}
             </div>
         ))}
       </div>
