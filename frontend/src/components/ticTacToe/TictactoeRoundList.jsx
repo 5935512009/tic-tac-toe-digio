d0dbe9de650b8af7b1ec8ffb3ec4569f88b094b3
5935512009 <Pasut2541@hotmail.com>
2024-07-11 16:13:30 +0700

    New:{basic show round list}

--- frontend/src/components/ticTacToe/TictactoeRoundList.jsx
+++ frontend/src/components/ticTacToe/TictactoeRoundList.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
-
+import './tictactoeRoundList.css'
 export default function TictactoeRoundList() {
     const [round,roundList]= useState([])
     useEffect(()=>{
@@ -8,15 +8,22 @@ export default function TictactoeRoundList() {
             .get("http://localhost:4001/api/Round")
             .then((res)=>{
                 roundList(res.data);
+                console.log(res.data);
             })
             .catch((error)=>{
                 console.log(error)
             })
-    })
+            
+    },[]);
   return (
     <div>
       play list 
-
+        {round.map((rounds,index)=>(
+            <div className='roundlist' key={index}>
+                <br/>
+                Round : {rounds.RoundId}  Size  : {rounds.Size} Time : {rounds.Timestamp}
+            </div>
+        ))}
     </div>
   )
 }
